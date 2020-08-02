a0ecdc2a1353caa48b815da0157a609c9d817009
Louie Jancevski <62314829+louiejancevski@users.noreply.github.com>
2020-08-01 23:09:25 -0400

    init (#5)
    Co-authored-by: Bill Liza <billliza@Bills-MacBook-Pro.local>

---
+++ routes/api/sorting.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const router = express.Router()
+
+//Models needed
+const Book = require('../../models/book')
+
+//@route GET api/sorting/:genre
+//@description Get single book by genre
+//@access Public
+router.get('/:genre', (req, res) => {
+    Book.find({genre : req.params.genre})
+        .then(book => res.json(book))
+        .catch(err => res.status(400).json({msg: 'Genre not available!'}));
+})
+
+//@route GET api/sorting/:author
+//@description Get single book by authors
+//@access Public
+router.get('/:author', (req, res) => {
+    Book.find({genre : req.params.author})
+        .then(book => res.json(book))
+        
