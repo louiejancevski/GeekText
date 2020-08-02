69f3a1a5f9481e4513c9d258f77870a907c3cde2
Javier <javierlopezg93@gamil.com>
2020-08-01 22:04:35 -0400

    Editing user completed

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,6 +1,6 @@
 // routes/api/users.js
-const express = require('express')
-const router = express.Router()
+const express = require('express') ;
+const router = express.Router() ;
 
 //Users Model
 const User = require('../../models/user')
@@ -41,19 +41,34 @@ router.post('/create', (req, res) => {
 // @route   POST api/users/create
 // @desc    Edit user
 // @access  Public
-router.edit('/:id', (req, res) => {
+router.post('/edit', (req, res) => {
   
+  const updatedId = req.body._id;
+  const updatedFName = req.body.firstName;
+  const updatedLName = req.body.lastName;
+  const updatedEmail = req.body.email;
+  const updatedCell = req.body.cellphone;
+  const updatedPassword = req.body.password;
 
-  User.findById(req.params.id)
-    .then(user => res.json({ 
-        msg: 'User added successfully',
-        user: user
-  }))
-    .catch(err => res.status(400).json({ 
-        msg: 'Unable to add this user',
-        err: err.message
-      }));
-});  
+  User.findById(updatedId).then( user => {
+    user.firstName = updatedFName;
+    user.lastName =updatedLName;
+    user.email = updatedEmail;
+    user.cellphone = updatedCell;
+    user.password = updatedPassword;
+    return user.save() ;
+
+  })
+  .then(user => res.json({ 
+          msg: 'User modified successfully',
+          user: user
+    }))
+      .catch(err => res.status(400).json({ 
+          msg: 'Unable to add this user',
+          err: err.message
+   })
+   );
+});
 
 // @router  DELETE api/users/:id
 // @desc    Delete user
